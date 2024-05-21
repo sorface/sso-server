b01e3ce559ff19bad16550ec12866be1dd13e6fe
Pavel Talaika <devpavdeveloper@yandex.ru>
2024-05-21 22:21:20 +0300

    feature/SF-62: (#18)
    - Add filter csrf

--- web/src/main/java/by/sorface/sso/web/records/responses/ProfileRecord.java
+++ web/src/main/java/by/sorface/sso/web/records/responses/ProfileRecord.java
@@ -1,7 +1,10 @@
 package by.sorface.sso.web.records.responses;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ProfileRecord(UUID id, String email, String firstName, String lastName, String avatar, List<String> roles) {
 }
