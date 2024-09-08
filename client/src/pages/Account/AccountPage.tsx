c7a31dfdd81100786ee2b379002fca858181017b
Павел Талайко <devpavdeveloper@yandex.ru>
2024-09-08 12:37:47 +0300

    feature/master - Add readiness and liveness probes

--- client/src/pages/Account/AccountPage.tsx
+++ client/src/pages/Account/AccountPage.tsx
@@ -1,13 +1,13 @@
-import React, { ChangeEvent, FunctionComponent, useContext, useEffect, useState } from 'react';
-import { Captions, IconNames } from "../../constants";
-import { Icon } from '../../components/Icon/Icon';
-import { AuthContext } from '../../context/AuthContext';
-import { LogoutForm } from '../../components/LogoutForm/LogoutForm';
-import { useApiMethodCsrf } from '../../hooks/useApiMethodCsrf';
-import { accountsApiDeclaration, EditAccountBody } from '../../apiDeclarations';
-import { Loader } from '../../components/Loader/Loader';
-import { UserAvatar } from '../../components/UserAvatar/UserAvatar';
-import { getUserAvatarCaption } from '../../utils/getUserAvatarCaption';
+import React, {ChangeEvent, FunctionComponent, useContext, useEffect, useState} from 'react';
+import {Captions, IconNames} from "../../constants";
+import {Icon} from '../../components/Icon/Icon';
+import {AuthContext} from '../../context/AuthContext';
+import {LogoutForm} from '../../components/LogoutForm/LogoutForm';
+import {useApiMethodCsrf} from '../../hooks/useApiMethodCsrf';
+import {accountsApiDeclaration, EditAccountBody} from '../../apiDeclarations';
+import {Loader} from '../../components/Loader/Loader';
+import {UserAvatar} from '../../components/UserAvatar/UserAvatar';
+import {getUserAvatarCaption} from '../../utils/getUserAvatarCaption';
 
 import './Account.css';
 
@@ -19,9 +19,9 @@ interface ProfileField {
 }
 
 export const AccountPage: FunctionComponent = () => {
-    const { account, loadAccount } = useContext(AuthContext);
-    const { fetchData, apiMethodState } = useApiMethodCsrf<unknown, EditAccountBody>(accountsApiDeclaration.edit);
-    const { data, process: { error, loading } } = apiMethodState;
+    const {account, loadAccount} = useContext(AuthContext);
+    const {fetchData, apiMethodState} = useApiMethodCsrf<unknown, EditAccountBody>(accountsApiDeclaration.edit);
+    const {data, process: {error, loading}} = apiMethodState;
     const [editedFieldName, setEditedFieldName] = useState('');
     const [editedFieldValue, setEditedFieldValue] = useState('');
 
@@ -66,11 +66,26 @@ export const AccountPage: FunctionComponent = () => {
         });
     };
 
+    const convertRole = (role: string): string | undefined => {
+        let mapRole: Map<string, string> = new Map<string, string>()
+
+        mapRole.set("ADMIN", "Admin");
+        mapRole.set("USER", "User");
+
+        return mapRole.get(role);
+    };
+
     const fields: ProfileField[] = [
         {
             name: 'id',
             caption: Captions.Id,
             value: account?.id,
+            editable: false,
+        },
+        {
+            name: 'username',
+            caption: Captions.Username,
+            value: account?.nickname,
             editable: false,
         },
         {
@@ -91,6 +106,12 @@ export const AccountPage: FunctionComponent = () => {
             value: account?.lastName,
             editable: true,
         },
+        {
+            name: 'roles',
+            caption: Captions.Roles,
+            value: account?.roles?.map(role => convertRole(role)).join(','),
+            editable: false,
+        },
     ];
 
     return (
@@ -101,9 +122,9 @@ export const AccountPage: FunctionComponent = () => {
                 src={account?.avatar}
             />
             {!!error && (<div>{Captions.Error}: {error}</div>)}
-            {!!loading && (<div><Loader /></div>)}
+            {!!loading && (<div><Loader/></div>)}
             <table className="user-data-table">
-                {fields.map(({ name, caption, value, editable }) => (
+                {fields.map(({name, caption, value, editable}) => (
                     <tr key={name}>
                         <td className="bold left">{caption}</td>
                         {name !== editedFieldName ? (
@@ -111,25 +132,26 @@ export const AccountPage: FunctionComponent = () => {
                                 <div className="field-value">{value || Captions.Unknown}</div>
                                 {editable && (
                                     <div className="field-action" onClick={() => handleEditField(name, value || '')}>
-                                        <Icon name={IconNames.Create} />
+                                        <Icon name={IconNames.Create}/>
                                     </div>
                                 )}
                             </td>
                         ) : (
                             <td className="right">
-                                <input className="field-value" type="text" value={editedFieldValue} onChange={handleEditedFieldChangeValue} />
+                                <input className="field-value" type="text" value={editedFieldValue}
+                                       onChange={handleEditedFieldChangeValue}/>
                                 <div className="field-action" onClick={handleEditedFieldSave}>
-                                    <Icon name={IconNames.Checkmark} />
+                                    <Icon name={IconNames.Checkmark}/>
                                 </div>
                                 <div className="field-action" onClick={handleEditedFieldCancel}>
-                                    <Icon name={IconNames.Close} />
+                                    <Icon name={IconNames.Close}/>
                                 </div>
                             </td>
                         )}
                     </tr>
                 ))}
             </table>
-            <LogoutForm />
+            <LogoutForm/>
         </div>
     );
 };
